cc86ffa69e76b1e7dd6bbeddbd4a794f95251535
Oleksii Krasnovskyi <finchatticu33@gmail.com>
2021-10-13 22:09:01 +0300

    add comments; patches

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,22 @@
 #include "calculator.h"
+/*
+ * Some sort of changes
+ * Think of it like an useless javadoc-like comments
+ */
 
+// Function that adds two numbers 
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
 }
 
+// Function that subtracts two numbers
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// Function that multiply two numbers
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
